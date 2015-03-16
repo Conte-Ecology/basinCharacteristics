46c31d0ba01e6b68e80976dc93e5e36603624d02
Tom-Bombadil <koneil@usgs.gov>
2015-03-16 11:15:14 -0400

    update soil drainage class

--- soilDrainageClass/scripts/soilDrainageClass.py
+++ soilDrainageClass/scripts/soilDrainageClass.py
@@ -1,35 +1,16 @@
 import arcpy
+from arcpy import env
+from arcpy.sa import *
 
 # ==============
 # Specify inputs
 # ==============
 
-baseDirectory = "C:/KPONEIL/GitHub/projects/basinCharacteristics/soilHydrologicGroup"
+baseDirectory = "C:/KPONEIL/GitHub/projects/basinCharacteristics/soilDrainageClass"
 states = ["MA", "CT", "RI", "ME", "NH", "VT", "NY", "DE", "MD", "NJ", "PA", "VA", "WV", "DC"]
-soilsFolder = "F:/KPONEIL/SourceData/geology/SSURGO"
+states = ["MA", "CT"]
+soilsFolder = "C:/KPONEIL/data/land/nrcsSSURGO/spatial"
 outputName = "Northeast"
-
-
-# Hydrologic group Combos	
-a =		""" "HYDGRP" = 'A' """
-
-ab = 	""" "HYDGRP" = 'A' OR 
-			"HYDGRP" = 'B' """
-	
-cd = 	""" "HYDGRP" = 'C' OR 
-			"HYDGRP" = 'D' OR 
-			"HYDGRP" = 'C/D' """
-
-d1 = 	""" "HYDGRP" = 'D' """
-
-d4 =	""" "HYDGRP" = 'A/D' OR 
-			"HYDGRP" = 'B/D' OR 
-			"HYDGRP" = 'C/D' OR
-			"HYDGRP" = 'D' """
-	
-grpList = [a, ab, cd, d1, d4]
-grpListNames = ["a", "ab", "cd", "d1", "d4"]
-
 
 # ===========
 # Folder prep
@@ -70,36 +51,37 @@ mxd = arcpy.mapping.MapDocument("CURRENT")
 df = arcpy.mapping.ListDataFrames(mxd)[0]
 
 
+#
+## I don't think we need the range raster for this type as no values get removed from drainage class...
+#
 
 # ===================
 # Create Range Raster
 # ===================
 
-# Create a list of the state polygons
-statePolyList = []
-for k in range(len(states)):
-	statePolyList.append(soilsFolder + "/" + "gssurgo_g_" + states[k] + ".gdb/SAPOLYGON")
+## Create a list of the state polygons
+##for k in range(len(states)):
+#	statePolyList.append(soilsFolder + "/" + "gssurgo_g_" + states[k] + ".gdb/SAPOLYGON")
+#
+## Merge state boundaries
+#arcpy.Merge_management(statePolyList, vectorDB + "/SoilsStates")
+#
+## Create regional outline
+#arcpy.Dissolve_management(vectorDB + "/SoilsStates", vectorDB + "/SoilsRange","#", "#", "SINGLE_PART", "DISSOLVE_LINES")
+#
+## Calculate the field that determines the raster value
+#arcpy.AddField_management("SoilsRange", "rasterVal", "SHORT")
+#arcpy.CalculateField_management ("SoilsRange", "rasterVal", 0, "PYTHON_9.3")	
+#
+## Create template for the final raster
+#arcpy.PolygonToRaster_conversion("SoilsRange", 
+#									"rasterVal", 
+#									rasterFolder + "/rangeRaster", 
+#									"MAXIMUM_COMBINED_AREA", 
+#									"NONE", 
+#									30)
 
-# Merge state boundaries
-arcpy.Merge_management(statePolyList, vectorDB + "/SoilsStates")
 
-# Create regional outline
-arcpy.Dissolve_management(vectorDB + "/SoilsStates", vectorDB + "/SoilsRange","#", "#", "SINGLE_PART", "DISSOLVE_LINES")
-
-# Calculate the field that determines the raster value
-arcpy.AddField_management("SoilsRange", "rasterVal", "SHORT")
-arcpy.CalculateField_management ("SoilsRange", "rasterVal", 0, "PYTHON_9.3")	
-
-# Create template for the final raster
-arcpy.PolygonToRaster_conversion("SoilsRange", 
-									"rasterVal", 
-									rasterFolder + "/rangeRaster", 
-									"MAXIMUM_COMBINED_AREA", 
-									"NONE", 
-									30)
-
-
-										
 # ========================
 # Create the state rasters
 # ========================
@@ -110,10 +92,10 @@ for i in range(len(states)):
 													vectorDB, 
 													"MUPOLYGON_" + states[i])
 
-	# Add the field that will be taken from the tables
-	arcpy.AddField_management("MUPOLYGON_" + states[i], 
-								"hydro_grp", 
-								"TEXT")
+	## Add the field that will be taken from the tables
+	#arcpy.AddField_management("MUPOLYGON_" + states[i], 
+	#							"DrainClass", 
+	#							"TEXT")
 
 
 	# Join "component" table to the polygon
@@ -128,75 +110,103 @@ for i in range(len(states)):
 	arcpy.AddJoin_management("MUPOLYGON_" + states[i], "mukey", "component" + "_" + states[i], "mukey")
 
 	
-	# Generate polygon of desired classifications
-	# -------------------------------------------
-	# Calculate the texture field in the Mapunit polygon
-	arcpy.CalculateField_management ("MUPOLYGON_" + states[i], "hydro_grp", "!hydgrp!", "PYTHON_9.3")	
+	# Generate state rasters
+	# ----------------------
+	# Calculate the Drainage Class field in the Mapunit polygon
+	#arcpy.CalculateField_management ("MUPOLYGON_" + states[i], "DrainClass", "!drainagecl!", "PYTHON_9.3")	
 
 	#
-	##
-	### In Progress.....
-	##
+	## Might be able to skip the step where a new field is calculated
 	#
-	
-	grpListNames
-	
-	for j in range(len(grpList)):
-	
-		# Select out the categories for Hydrologic Group classifications
-		statePolyGrp = arcpy.FeatureClassToFeatureClass_conversion (vectorDB + "/MUPOLYGON_" + states[i], 
-																		vectorDB, 
-																		"hydgrp_" + grpListNames[j] + "_" + states[i], 
-																		grp[j])
 
-		# Rasterize the state polygon
-		# ---------------------------
-		# Calculate the field that determines the raster value
-		arcpy.AddField_management(statePolyGrp, "rasterVal", "SHORT")
-		arcpy.CalculateField_management (statePolyGrp, "rasterVal", 1, "PYTHON_9.3")		
-					
-		# Set the extent																			 
-		arcpy.env.extent = rasterFolder + "/rangeRaster"																			 
-
-		# Convert to raster
-		arcpy.PolygonToRaster_conversion(statePolyGrp, 
-											"rasterVal", 
-											rasterFolder + "/hydgrp_" + grpListNames[j] + "_" + states[i], 
+	# Convert to raster
+	arcpy.PolygonToRaster_conversion("MUPOLYGON_" + states[i], 
+											"component_" + states[i] + ".drainagecl", 
+											rasterFolder + "/DrnCls_" + states[i], 
 											"MAXIMUM_COMBINED_AREA", 
 											"NONE", 
 											30)
 											
-		# Remove some layers from the map
-		# -------------------------------
-		arcpy.mapping.RemoveLayer(df, arcpy.mapping.ListLayers(mxd, "hydgrp_" + grpListNames[j] + "_" + states[i], df)[0] )
-		arcpy.mapping.RemoveLayer(df, arcpy.mapping.ListLayers(mxd, "MUPOLYGON_"  + states[i], df)[0] )
+	# Remove some layers from the map
+	# -------------------------------
+	arcpy.mapping.RemoveLayer(df, arcpy.mapping.ListLayers(mxd, "DrnCls_" + states[i], df)[0] )
+	arcpy.mapping.RemoveLayer(df, arcpy.mapping.ListLayers(mxd, "MUPOLYGON_"  + states[i], df)[0] )
 
 # End state loop
+
 
 
 # ==================										
 # Mosaic the rasters
 # ==================										
 
-# Loop through each hydrologic group classification
-# -------------------------------------------------
-for k in range(len(grpList)):
 
-	mosaicList = [rasterFolder + "/rangeRaster"]
+mosaicList = []
 		
-	for s in range(len(states)): 
-		mosaicList.append(rasterFolder + "/hydgrp_" + grpListNames[j] + "_" + states[i])
-	del s
+for s in range(len(states)): 
+	mosaicList.append(rasterFolder + "/DrnCls_" + states[s])
+del s
+
+#Since there isn't a range raster, not entire sure how mosaicking will work...
 		
-	# Set processing extent for rasterization
-	arcpy.env.extent = rasterFolder + "/rangeRaster"
+## Set processing extent for rasterization
+#arcpy.env.extent = rasterFolder + "/rangeRaster"
 		
-	arcpy.MosaicToNewRaster_management(mosaicList,
-										outputFolder, 
-										"surfCoarse",
-										rasterFolder + "/rangeRaster",
-										"8_BIT_UNSIGNED", 
-										30, 
-										1, 
-										"MAXIMUM",
-										"FIRST")
+arcpy.MosaicToNewRaster_management(mosaicList,
+									rasterFolder, 
+									"rawDrnCls",
+									"#",
+									"8_BIT_UNSIGNED", 
+									30, 
+									1, 
+									"MAXIMUM",
+									"FIRST")
+
+									
+
+# Import system modules
+
+#
+## The reclassfication step needs to go within the loop, before mosaicking.
+#
+
+# Set environment settings
+#env.workspace = "C:/sapyexamples/data"
+
+# Set local variables
+inRaster = "rawDrnCls"
+reclassField = "DrainClass"
+remap = RemapValue([["Excessively drained", 1], 
+					["Somewhat excessively drained", 2],
+					["Well drained", 3],
+					["Moderately well drained", 4],
+					["Somewhat poorly drained", 5],
+					["Poorly drained", 6],
+					["Very poorly drained", 7]])
+
+# Check out the ArcGIS Spatial Analyst extension license
+#arcpy.CheckOutExtension("Spatial")
+
+# Execute Reclassify
+outReclassify = Reclassify(rasterFolder + "/rawDrnCls", reclassField, remap, "NODATA")
+
+# Save the output 
+outReclassify.save(outputFolder + "/drainclass")
+
+
+
+
+
+									
+arcpy.AddField_management("DrainVal", "rasterVal", "SHORT")
+arcpy.CalculateField_management ("SoilsRange", "rasterVal", 0, "PYTHON_9.3")	
+									
+1 = Excessively drained
+2 = Somewhat excessively drained
+3 = Well drained
+4 = Moderately well drained
+5 = Somewhat poorly drained
+6 = Poorly drained
+7 = Very poorly drained				
+									
+									
